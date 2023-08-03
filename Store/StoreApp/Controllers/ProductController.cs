1c500295082aac65790e56766d166447fb89d091
Umut <umutbahur@gmail.com>
2023-08-03 23:30:06 +0300

    Layout has been created.

---
+++ Store/StoreApp/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StoreApp.Models;
+
+namespace StoreApp.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly RepositoryContext _context;
+
+        public ProductController(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+           /* var context = new RepositoryContext(
+            new DbContextOptionsBuilder<RepositoryContext>()
+            .UseSqlite("Data Source = C:\\Users\\HP\\Desktop\\MVC\\ProductDb.db")
+            .Options);*/
+
+            var model = _context.Products.ToList();
+            return View(model);
+        }
+
+        public IActionResult Get(int id)
+        {
+            Product product = _context.Products.First(p=> p.ProductId.Equals(id));
+            return View(product);
+        }
+    }
+}
